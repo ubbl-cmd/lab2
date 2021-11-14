f60883d93a9138d043dff1b3ee438c1892f656d3
ubbl-cmd <aleksej.sapojnikov@gmail.com>
2021-11-14 18:36:59 +0300

    try3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	gcc abs_max.c abs_min.c diff.c sum.c menu.c -o menu
+	gcc -std=c99 abs_max.c abs_min.c diff.c sum.c menu.c -o menu
 clean:
 	rm -rf *.o hello
--- abs_min.c
+++ abs_min.c
@@ -6,7 +6,7 @@ int abs_min(int* array, int len) {
     int abs_min = abs(array[0]);
     int result = array[0];
     for (int i = 1; i < len; i++) {
-        if (abs(array[i]) < abs_max) {
+        if (abs(array[i]) < abs_min) {
             abs_min = abs(array[i]);
             result = array[i];
         }
